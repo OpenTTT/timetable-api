8eacbc8d9852a7974107a1865befac37a3c44977
Patrick Stegmann <patrick.stegmann@zuehlke.com>
2019-01-30 11:30:23 +0100

    Some documentation, removed TODOs

--- src/main/java/org/openttd/opentttimetables/model/ScheduledOrder.java
+++ src/main/java/org/openttd/opentttimetables/model/ScheduledOrder.java
@@ -1,15 +1,19 @@
 package org.openttd.opentttimetables.model;
 
-import lombok.Data;
-
 import java.time.LocalTime;
 
 /**
  * A single order in a Schedule
  */
-@Data
 public class ScheduledOrder {
+    /**
+     * The underlying order, containing the actual routing and timetabling information.
+     */
     private TimetabledOrder order;
+
+    /**
+     * The arrival time from the previous {@link ScheduledOrder} (or start of the {@link Schedule}, if there isn't one).
+     */
     private LocalTime arrival;
 
     public ScheduledOrder(TimetabledOrder order, LocalTime arrival) {
@@ -21,6 +25,9 @@ public class ScheduledOrder {
         return arrival;
     }
 
+    /**
+     * @return The calculated departure time based on the staying time of the {@link TimetabledOrder}
+     */
     public LocalTime getDeparture() {
         return arrival.plusMinutes(order.getStayingTime());
     }
