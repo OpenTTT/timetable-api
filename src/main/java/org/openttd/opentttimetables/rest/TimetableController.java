81ca267e967c979666cc9368c1679c87bfaad81b
Patrick Stegmann <patrick.stegmann@zuehlke.com>
2019-01-31 23:09:12 +0100

    Timetable controller decluttering

--- src/main/java/org/openttd/opentttimetables/rest/TimetableController.java
+++ src/main/java/org/openttd/opentttimetables/rest/TimetableController.java
@@ -1,10 +1,12 @@
 package org.openttd.opentttimetables.rest;
 
 import org.modelmapper.ModelMapper;
+import org.openttd.opentttimetables.model.Destination;
 import org.openttd.opentttimetables.model.Timetable;
 import org.openttd.opentttimetables.model.TimetabledOrder;
 import org.openttd.opentttimetables.repo.DestinationRepo;
 import org.openttd.opentttimetables.repo.TimetableRepo;
+import org.openttd.opentttimetables.rest.dto.MapperService;
 import org.openttd.opentttimetables.rest.dto.TimetableDTO;
 import org.openttd.opentttimetables.rest.dto.TimetabledOrderDTO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,13 +16,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @RestController
 public class TimetableController {
     @Autowired
-    private ModelMapper mapper;
+    private MapperService mapper;
 
     @Autowired
     private TimetableRepo timetableRepo;
@@ -29,47 +32,14 @@ public class TimetableController {
     private DestinationRepo destinationRepo;
 
     @RequestMapping(path = {"/timetable", "/timetables"})
-    public Iterable<TimetableDTO> getAllTimetables() {
-        return StreamSupport.stream(timetableRepo.findAll().spliterator(), true)
-                .map(this::toDto)
-                .collect(Collectors.toList());
+    public List<TimetableDTO> getAllTimetables() {
+        return mapper.mapAll(timetableRepo.findAll(), TimetableDTO.class);
     }
 
     @RequestMapping(method = RequestMethod.POST, path = "/timetable")
     public TimetableDTO createNewTimetable(@RequestBody @Valid TimetableDTO dto) {
-        Timetable timetable = timetableRepo.save(fromDto(dto));
-        return toDto(timetable);
-    }
-
-    private TimetableDTO toDto(Timetable timetable) {
-        TimetableDTO dto = mapper.map(timetable, TimetableDTO.class);
-        dto.setOrders(timetable.getOrders().stream()
-                .map(this::toDto)
-                .collect(Collectors.toList())
-        );
-        return dto;
-    }
-
-    private TimetabledOrderDTO toDto(TimetabledOrder order) {
-        TimetabledOrderDTO dto = mapper.map(order, TimetabledOrderDTO.class);
-        dto.setDestination(order.getDestination().getName());
-        return dto;
-    }
-
-    private Timetable fromDto(TimetableDTO dto) {
         Timetable timetable = mapper.map(dto, Timetable.class);
-        timetable.setOrders(dto
-                .getOrders()
-                .stream()
-                .map(this::fromDto)
-                .collect(Collectors.toList()));
-        return timetable;
+        timetable.setOrders(dto.mapOrders(mapper, destinationRepo));
+        return mapper.map(timetableRepo.save(timetable), TimetableDTO.class);
     }
-
-    private TimetabledOrder fromDto(TimetabledOrderDTO dto) {
-        TimetabledOrder order = mapper.map(dto, TimetabledOrder.class);
-        order.setDestination(destinationRepo.findByName(dto.getDestination()));
-        return order;
-    }
-
 }
--- src/main/java/org/openttd/opentttimetables/rest/dto/TimetableDTO.java
+++ src/main/java/org/openttd/opentttimetables/rest/dto/TimetableDTO.java
@@ -2,12 +2,24 @@ package org.openttd.opentttimetables.rest.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.openttd.opentttimetables.model.TimetabledOrder;
+import org.openttd.opentttimetables.repo.DestinationRepo;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
 public class TimetableDTO {
     private String name;
     private List<TimetabledOrderDTO> orders;
+
+    public List<TimetabledOrder> mapOrders(MapperService mapper, DestinationRepo destinationRepo) {
+        return orders.stream()
+                .map(orderDto -> {
+                    TimetabledOrder timetabledOrder = mapper.map(orderDto, TimetabledOrder.class);
+                    timetabledOrder.setDestination(destinationRepo.findByName(orderDto.getDestination()));
+                    return timetabledOrder;
+                }).collect(Collectors.toList());
+    }
 }
