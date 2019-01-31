72a2c6c5d8d076c1bb71e995a7a4565e3241329e
Patrick Stegmann <patrick.stegmann@zuehlke.com>
2019-01-31 22:01:28 +0100

    Added a stats endpoint which gives some... well, stats!

---
+++ src/main/java/org/openttd/opentttimetables/rest/StatsController.java
@@ -0,0 +1,37 @@
+package org.openttd.opentttimetables.rest;
+
+import com.google.common.collect.Lists;
+import org.openttd.opentttimetables.model.ScheduledDispatch;
+import org.openttd.opentttimetables.repo.DestinationRepo;
+import org.openttd.opentttimetables.repo.ScheduledDispatchRepo;
+import org.openttd.opentttimetables.repo.TimetableRepo;
+import org.openttd.opentttimetables.rest.dto.StatsDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class StatsController {
+    @Autowired
+    private DestinationRepo destinationRepo;
+
+    @Autowired
+    private ScheduledDispatchRepo scheduledDispatchRepo;
+
+    @Autowired
+    private TimetableRepo timetableRepo;
+
+    @RequestMapping("/stats")
+    public StatsDTO getStats() {
+        long totalDestinations = destinationRepo.count();
+        long totalTimetables = timetableRepo.count();
+
+        ArrayList<ScheduledDispatch> scheduledDispatches = Lists.newArrayList(scheduledDispatchRepo.findAll());
+        long totalDispatches = scheduledDispatches.size();
+        long totalDepatures = scheduledDispatches.stream().mapToInt(sd -> sd.getDepartures().size()).sum();
+
+        return new StatsDTO(totalDestinations, totalDispatches, totalDepatures, totalTimetables);
+    }
+}
