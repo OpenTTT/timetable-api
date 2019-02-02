8c27cd541f86eab9a2db987bfdbe3d18ccbb5c86
Patrick Stegmann <patrick.stegmann@zuehlke.com>
2019-02-02 18:01:53 +0100

    Disallow too high and negative values for TimetabledOrder traveling/staying times

--- src/main/java/org/openttd/opentttimetables/rest/dto/TimetabledOrderDTO.java
+++ src/main/java/org/openttd/opentttimetables/rest/dto/TimetabledOrderDTO.java
@@ -3,12 +3,21 @@ package org.openttd.opentttimetables.rest.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 @Getter
 @Setter
 public class TimetabledOrderDTO {
     private Integer id;
     private String destination;
+
+    @Min(0)
+    @Max(256)
     private Integer stayingTime;
+
+    @Min(0)
+    @Max(256)
     private Integer travelingTime;
     private boolean returnOrder = false;
 }
