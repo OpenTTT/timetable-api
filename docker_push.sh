f95074189a1c4e33fa4fc3f77bcc78e5f8eba392
Patrick Stegmann <patrick.stegmann@zuehlke.com>
2019-02-04 16:19:22 +0100

    Trying to push a docker image from TravisCI

---
+++ docker_push.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+# This script is meant to be exlusively run by travis-ci.
+# Password and username will be set by build environment.
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push openttt/openttt-backend
