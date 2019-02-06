8648afcf59713ac0fc699f2b6c528d51ff9068c0
Patrick Stegmann <patrick.a.stegmann@gmail.com>
2019-02-06 22:05:23 +0100

    Introduced swagger + ui

--- src/main/java/org/openttd/opentttimetables/WebConfig.java
+++ src/main/java/org/openttd/opentttimetables/WebConfig.java
@@ -3,6 +3,7 @@ package org.openttd.opentttimetables;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -14,5 +15,13 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+		registry.addResourceHandler("/webjars/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 }
